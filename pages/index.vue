4f0f5387db5369a8d66219788fe936804b3b9bc9
Danil Kireev <unrndm@gmail.com>
2024-04-23 19:33:40 +0300

    feat: started working on main page

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,14 @@
     <HeroBanner text="Кафедра системных исследований" />
 
     <div class="mx-auto max-w-7xl h-screen">
-      <h2>Главная страница</h2>
+      <p>
+        Кафедра системных исследований является базовой кафедрой
+        <a
+          class="underline text-blue-500 hover:text-blue-700"
+          href="https://fbmf.mipt.ru/education/basic-departments/kafedra-sistemnykh-issledovaniy"
+          >ФПМИ МФТИ</a
+        >
+      </p>
     </div>
   </div>
 </template>
